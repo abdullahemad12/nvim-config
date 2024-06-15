b0b40cea7722e0a65dfdf7a4c265bfcdcc092350
Abdullah Emad <abdullahem1997@gmail.com>
2024-06-15 15:54:40 +0200

    Add a function to switch between camel case and snake case

--- init.lua
+++ init.lua
@@ -164,6 +164,28 @@ function OnDemandCompletion()
   end
 end
 
+-- source: https://dev.to/dimaportenko/switching-between-camelcase-and-snakecase-in-neovim-using-lua-3ah7
+function switch_case()
+  print("triggered")
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local word = vim.fn.expand('<cword>')
+  local word_start = vim.fn.matchstrpos(vim.fn.getline('.'), '\\k*\\%' .. (col+1) .. 'c\\k*')[2]
+
+  -- Detect camelCase
+  if word:find('[a-z][A-Z]') then
+    -- Convert camelCase to snake_case
+    local snake_case_word = word:gsub('([a-z])([A-Z])', '%1_%2'):lower()
+    vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, {snake_case_word})
+  -- Detect snake_case
+  elseif word:find('_[a-z]') then
+    -- Convert snake_case to camelCase
+    local camel_case_word = word:gsub('(_)([a-z])', function(_, l) return l:upper() end)
+    vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_start + #word, {camel_case_word})
+  else
+    print("Not a snake_case or camelCase word")
+  end
+end
+
 -- Bind the completion function to a key mapping
 local opts =  { noremap = true, silent = true }
 vim.api.nvim_set_keymap('i', '<C-Space>', '<Esc>:lua OnDemandCompletion()<CR>a', opts) -- maps autocomplete to "ctrl-space"
@@ -177,6 +199,7 @@ vim.api.nvim_set_keymap('n', '<', ':bp<CR>', opts) -- maps going to the previous
 vim.api.nvim_set_keymap('n', '>', ':bn<CR>', opts) -- maps going to the next buffer to >
 vim.api.nvim_set_keymap('n', '<C-A-x>', ':bd<CR>', opts) -- maps deleting a buffer to ctrl-alt-x
 vim.api.nvim_set_keymap('n', '<C-A-p>', ':tabnew<CR>', opts) -- maps opening new tab to ctrl-alt-p
+vim.api.nvim_set_keymap('n', 'q', ':lua switch_case()<CR>', opts) -- switch between camel case and snake case
 
 -- run lint fix for ts and js on file save
 vim.cmd([[
